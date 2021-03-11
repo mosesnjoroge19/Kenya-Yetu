4ea9e8b757b03fb79321ae7739ee991f9d7d1f5d
mosesnjoroge19 <njorogemosesngure@gmail.com>
2021-03-11 20:33:25 +0300

    First commit for EUCOSSA Android Devs

---
+++ app/src/main/java/com/example/kenyayetu/firstscreen.java
@@ -0,0 +1,14 @@
+package com.example.kenyayetu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class firstscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstscreen);
+    }
+}
